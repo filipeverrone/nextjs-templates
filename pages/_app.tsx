c76b1722498c8b1d98bcc56fcc87802b20a5247e
Filipe Verrone de Lima <verronefilipe@gmail.com>
2022-09-27 06:28:45 -0300

    feat: eslint config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<Head>
+				<title>Next Templates</title>
+				{/* <link rel="icon" href="/favicon.ico" /> */}
+			</Head>
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
